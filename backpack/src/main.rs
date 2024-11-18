808576b34a1aac1513ec0af1c1c962078dd4f91e
Yonas <yonas.y@gmail.com>
2024-11-18 08:55:26 -0500

    feat: list repos of an organization.

--- backpack/src/main.rs
+++ backpack/src/main.rs
@@ -1,5 +1,51 @@
-use test2::*;
+use octocrab::Octocrab;
+use clap::Parser;
 
-fn main() {
-    println!("3 time 5 is {}", multiply(3, 5));
+/// List GitHub repos
+#[derive(Parser)]
+struct Cli {
+    /// The organization
+    #[arg(index = 1)]
+    org: Option<String>,  // Optional positional argument
+}
+
+#[tokio::main]
+async fn main() -> octocrab::Result<()> {
+    // Parse the command-line arguments
+    let cli = Cli::parse();
+
+    let token = std::env::var("GITHUB_TOKEN").expect("GITHUB_TOKEN env variable is required");
+    let octocrab = Octocrab::builder().personal_token(token).build()?;
+
+    // Get the value of the positional argument (if provided)
+    let page = match cli.org {
+        Some(org) => {
+            octocrab
+                .current()
+                .list_repos_for_authenticated_user(Some(String::from(org)))
+                .type_("owner")
+                .sort("updated")
+                .per_page(100)
+                .send()
+                .await?
+        },
+        None => {
+            octocrab
+                .current()
+                .list_repos_for_authenticated_user(None)
+                .type_("owner")
+                .sort("updated")
+                .per_page(100)
+                .send()
+                .await?
+        }
+    };
+
+    let results = octocrab.all_pages(page).await.unwrap();
+
+    for repo in results {
+        println!("{}, {}", repo.full_name.unwrap(), repo.default_branch.unwrap());
+    }
+
+    Ok(())
 }
